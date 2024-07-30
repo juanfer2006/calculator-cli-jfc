bffe243126a79df89636f6cd8bfbf861498229bc
Juan Fernando Castañeda <juanfercasta25@gmail.com>
2024-07-30 11:15:25 -0500

    Create add method Add subtract, multiply and divide methods

--- calculator/model.py
+++ calculator/model.py
@@ -1,3 +1,14 @@
 class Calculator:
-    def add(self, a: float, b: float)->float:
+    def add(self, a: float, b: float) -> float:
         return a + b
+
+    def subtract(self, a: float, b: float) -> float:
+        return a - b
+
+    def multiply(self, a: float, b: float) -> float:
+        return a * b
+
+    def divide(self, a: float, b: float) -> float:
+        if b == 0:
+            return "Cannot divide by 0!"
+        return a / b
